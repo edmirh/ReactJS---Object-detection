0973f1716335c54bd81560151a41d40dabbae3c0
edmirh <57446097+edmirh@users.noreply.github.com>
2020-07-01 11:44:06 +0200

    Edit server.js

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const cors = require('cors');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "edmir",
-	database: "results"
-});
 
 const app = express();
 const port = process.env.PORT || 6000;
@@ -20,14 +12,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
 	   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-con.connect();
-
-app.set('/results/test.txt', function(req, res) {
-	var detected = req.body;
-	var query = con.query('INSERT INTO board(Name) VALUES(?)', 'STM32F407', function(err, result) {});
-	res.end('Success');
 });
 
 app.listen(process.env.PORT || port);
